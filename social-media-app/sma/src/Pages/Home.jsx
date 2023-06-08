ca6566145f5e60e903d262c5616e47a68d7fce6b
carloskirui17 <129453813+carloskirui17@users.noreply.github.com>
2023-06-08 16:23:38 +0000

    done

--- social-media-app/sma/src/Pages/Home.jsx
+++ social-media-app/sma/src/Pages/Home.jsx
@@ -1,9 +1,18 @@
-import React from 'react'
-
-export  function Home() {
+import * as React from 'react'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faHeart } from '@fortawesome/free-solid-svg-icons'
+export function Home() {
   return (
     <>
-        Home
+      <div className=''>
+        <div>
+          <ul>
+            <li>
+            <FontAwesomeIcon icon={faHeart} size="lg" style={{color: "#ff0000",}} />
+            </li>
+          </ul>
+        </div>
+      </div>
     </>
   )
 }
